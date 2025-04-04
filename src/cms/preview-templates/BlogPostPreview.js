694564f4a27e4ad8f98176aeb1e9de0438b2d7db
Tushar <tushar.b@esmagico.in>
2025-04-04 12:14:11 +0530

    blog-post bugFix

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,22 +2,36 @@ import { BlogPostTemplate } from "../../templates/blog-post";
 import PropTypes from "prop-types";
 import React from "react";
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <>
-    <BlogPostTemplate
-      htmlContent={widgetFor("body")}
-      content={{
-        title: entry.getIn(["data", "title"]),
-        authorImage: entry.getIn(["data", "authorImage"]),
-        author: entry.getIn(["data", "author"]),
-        date: widgetFor("date")
-      }}
-      description={entry.getIn(["data", "description"])}
-      tags={entry.getIn(["data", "tags"])}
-      title={entry.getIn(["data", "title"])}
-    />
-  </>
-);
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  const authorImageData = entry.getIn(["data", "authorImage"]);
+  const authorImage = authorImageData ? {
+    childImageSharp: {
+      fluid: {
+        src: authorImageData
+      }
+    }
+  } : null;
+  return(
+   <>
+     <BlogPostTemplate
+       htmlContent={widgetFor("body")}
+       content={{
+         title: entry.getIn(["data", "title"]),
+         authorImage: authorImage ,
+         author: entry.getIn(["data", "author"]),
+         date: widgetFor("date")
+       }}
+       description={entry.getIn(["data", "description"])}
+       tags={entry.getIn(["data", "tags"])}
+       title={entry.getIn(["data", "title"])}
+     />
+   </>
+  ) 
+}
+
+ 
+ 
+
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
